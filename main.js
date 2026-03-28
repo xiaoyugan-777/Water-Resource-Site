b2ab7dc99a0f8567c4b64b479b3f667349d08c85
fang0575 <fang0575@umn.edu>
2026-03-28 11:43:13 -1000

    Initial site copy

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-grid");
+
+if (menuButton && nav) {
+  menuButton.addEventListener("click", () => {
+    const isOpen = nav.classList.toggle("is-open");
+    menuButton.setAttribute("aria-expanded", String(isOpen));
+  });
+}
